016d6eee1e3d2369d8a7b12a093ca3407b050773
swedendrift <swedendrift@gmail.com>
2017-06-28 09:32:08 -0700

    basic apache install

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic cookbook used with OpsWorks for Sturdy Networks skills test.
---
+++ recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: simple_demo
+# Recipe:: default
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "apt-get update" do
+    command "apt-get update"
+end
+
+package "apache2" do
+    action :install
+end
+
+service "apache2" do
+    action [:start, :enable]
+end
+
+cookbook_file "/var/www/index.html" do
+    source "index.html"
+    mode "0644"
+end
